4c4a410bb5ed1107757b21d5be276d91276029d5
elegracer <elegracer@gmail.com>
2019-09-01 16:52:08 +0800

    Add nanogui, nanovis

---
+++ depends/nanogui.cmake
@@ -0,0 +1,21 @@
+if(NOT TARGET depends::nanogui)
+    FetchContent_Declare(
+        depends-nanogui
+        GIT_REPOSITORY https://github.com/elegracer/nanogui.git
+        GIT_TAG        master
+    )
+    FetchContent_GetProperties(depends-nanogui)
+    if(NOT depends-nanogui_POPULATED)
+        message(STATUS "Fetching nanogui sources")
+        FetchContent_Populate(depends-nanogui)
+        message(STATUS "Fetching nanogui sources - done")
+    endif()
+    set(NANOGUI_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
+    add_subdirectory(${depends-nanogui_SOURCE_DIR} ${depends-nanogui_BINARY_DIR})
+    add_library(depends::nanogui INTERFACE IMPORTED GLOBAL)
+    target_link_libraries(depends::nanogui INTERFACE nanogui)
+    set(depends-nanogui-source-dir ${depends-nanogui_SOURCE_DIR} CACHE INTERNAL "" FORCE)
+    set(depends-nanogui-binary-dir ${depends-nanogui_BINARY_DIR} CACHE INTERNAL "" FORCE)
+    mark_as_advanced(depends-nanogui-source-dir)
+    mark_as_advanced(depends-nanogui-binary-dir)
+endif()
---
+++ depends/nanovis.cmake
@@ -0,0 +1,21 @@
+if(NOT TARGET depends::nanovis)
+    FetchContent_Declare(
+        depends-nanovis
+        GIT_REPOSITORY https://github.com/elegracer/nanovis.git
+        GIT_TAG        master
+    )
+    FetchContent_GetProperties(depends-nanovis)
+    if(NOT depends-nanovis_POPULATED)
+        message(STATUS "Fetching nanovis sources")
+        FetchContent_Populate(depends-nanovis)
+        message(STATUS "Fetching nanovis sources - done")
+    endif()
+    set(NANOVIS_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
+    add_subdirectory(${depends-nanovis_SOURCE_DIR} ${depends-nanovis_BINARY_DIR})
+    add_library(depends::nanovis INTERFACE IMPORTED GLOBAL)
+    target_link_libraries(depends::nanovis INTERFACE nanovis)
+    set(depends-nanovis-source-dir ${depends-nanovis_SOURCE_DIR} CACHE INTERNAL "" FORCE)
+    set(depends-nanovis-binary-dir ${depends-nanovis_BINARY_DIR} CACHE INTERNAL "" FORCE)
+    mark_as_advanced(depends-nanovis-source-dir)
+    mark_as_advanced(depends-nanovis-binary-dir)
+endif()
