8e401e4dd13d0719ee6d4046dbf399e1356aff0e
elegracer <elegracer@gmail.com>
2019-09-01 18:00:21 +0800

    Check local opencv version

--- externs/opencv.cmake
+++ externs/opencv.cmake
@@ -1,5 +1,8 @@
 if(NOT TARGET depends::opencv)
     find_package(OpenCV REQUIRED)
+    if (NOT ${OpenCV_VERSION} STREQUAL "4.1.1")
+        message(FATAL_ERROR "OpenCV version is not 4.1.1, please update OpenCV to 4.1.1")
+    endif()
     add_library(depends::opencv INTERFACE IMPORTED GLOBAL)
     target_link_libraries(depends::opencv INTERFACE ${OpenCV_LIBS})
 endif()
